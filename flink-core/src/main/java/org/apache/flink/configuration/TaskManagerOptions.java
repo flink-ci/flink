b606308e642fc66063d5ac4ce30133395526e324
Xintong Song <tonysong820@gmail.com>
2019-11-08 09:59:24 +0800

    [FLINK-14637][core] Intorduce config option for framework off-heap memory.

--- flink-core/src/main/java/org/apache/flink/configuration/TaskManagerOptions.java
+++ flink-core/src/main/java/org/apache/flink/configuration/TaskManagerOptions.java
@@ -279,6 +279,16 @@ public class TaskManagerOptions {
 				+ " for TaskExecutor framework, which will not be allocated to task slots.");
 
 	/**
+	 * Framework Off-Heap Memory size for TaskExecutors.
+	 */
+	public static final ConfigOption<String> FRAMEWORK_OFF_HEAP_MEMORY =
+		key("taskmanager.memory.framework.off-heap.size")
+			.defaultValue("16m")
+			.withDescription("Framework Off-Heap Memory size for TaskExecutors. This is the size of off-heap memory"
+				+ " (JVM direct memory or native memory) reserved for TaskExecutor framework, which will not be"
+				+ " allocated to task slots.");
+
+	/**
 	 * Task Heap Memory size for TaskExecutors.
 	 */
 	public static final ConfigOption<String> TASK_HEAP_MEMORY =
