2e9c4799e0b184a0bf522bfdd80c9645c69d3d24
Xintong Song <tonysong820@gmail.com>
2019-11-23 14:43:03 +0800

    [FLINK-13983][runtime][yarn/mesos][dist] Launch task executor with new memory calculation logics
        - TM startup scripts calls java code to set flip49 TM resource configs and JVM parameters
        - Launches TaskExecutors on Yarn/Mesos with JVM parameters and dynamic configs generated from TaskExecutorResourceSpec
        - Use TaskExecutorResourceSpec to calculate memory for ShuffleEnvironment and MemoryManager in TM

--- flink-runtime/src/test/java/org/apache/flink/runtime/clusterframework/TaskExecutorResourceUtilsTest.java
+++ flink-runtime/src/test/java/org/apache/flink/runtime/clusterframework/TaskExecutorResourceUtilsTest.java
@@ -44,8 +44,8 @@ public class TaskExecutorResourceUtilsTest extends TestLogger {
 
 	private static final MemorySize TASK_HEAP_SIZE = MemorySize.parse("100m");
 	private static final MemorySize MANAGED_MEM_SIZE = MemorySize.parse("200m");
-	private static final MemorySize TOTAL_FLINK_MEM_SIZE = MemorySize.parse("900m");
-	private static final MemorySize TOTAL_PROCESS_MEM_SIZE = MemorySize.parse("1g");
+	private static final MemorySize TOTAL_FLINK_MEM_SIZE = MemorySize.parse("1280m");
+	private static final MemorySize TOTAL_PROCESS_MEM_SIZE = MemorySize.parse("1536m");
 
 	private static final TaskExecutorResourceSpec TM_RESOURCE_SPEC = new TaskExecutorResourceSpec(
 		MemorySize.parse("1m"),
@@ -474,7 +474,7 @@ public class TaskExecutorResourceUtilsTest extends TestLogger {
 
 	@Test
 	public void testConfigTotalProcessMemoryLegacyMB() {
-		final MemorySize totalProcessMemorySize = MemorySize.parse("1g");
+		final MemorySize totalProcessMemorySize = MemorySize.parse("2g");
 
 		@SuppressWarnings("deprecation")
 		final ConfigOption<Integer> legacyOption = TaskManagerOptions.TASK_MANAGER_HEAP_MEMORY_MB;
@@ -488,7 +488,7 @@ public class TaskExecutorResourceUtilsTest extends TestLogger {
 
 	@Test
 	public void testConfigTotalProcessMemorySize() {
-		final MemorySize totalProcessMemorySize = MemorySize.parse("1g");
+		final MemorySize totalProcessMemorySize = MemorySize.parse("2g");
 
 		Configuration conf = new Configuration();
 		conf.setString(TaskManagerOptions.TOTAL_PROCESS_MEMORY, totalProcessMemorySize.getMebiBytes() + "m");
@@ -512,7 +512,7 @@ public class TaskExecutorResourceUtilsTest extends TestLogger {
 
 	@Test
 	public void testConfigTotalProcessMemoryLegacySize() {
-		final MemorySize totalProcessMemorySize = MemorySize.parse("1g");
+		final MemorySize totalProcessMemorySize = MemorySize.parse("2g");
 
 		@SuppressWarnings("deprecation")
 		final ConfigOption<String> legacyOption = TaskManagerOptions.TASK_MANAGER_HEAP_MEMORY;
