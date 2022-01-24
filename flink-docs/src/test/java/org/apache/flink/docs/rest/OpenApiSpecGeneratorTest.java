eff23fa785b767fd2622d12a7552b77863330de0
Chesnay Schepler <chesnay@apache.org>
2022-01-24 14:15:44 +0100

    migrate test to junit5

--- flink-docs/src/test/java/org/apache/flink/docs/rest/OpenApiSpecGeneratorTest.java
+++ flink-docs/src/test/java/org/apache/flink/docs/rest/OpenApiSpecGeneratorTest.java
@@ -29,43 +29,35 @@ import org.apache.flink.util.TestLogger;
 
 import org.apache.flink.shaded.netty4.io.netty.channel.ChannelInboundHandler;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.CoreMatchers.not;
-import static org.junit.Assert.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-/** Tests for the {@link OpenApiSpecGenerator}. */
-public class OpenApiSpecGeneratorTest extends TestLogger {
+class OpenApiSpecGeneratorTest extends TestLogger {
 
     @Test
-    public void testExcludeFromDocumentation() throws Exception {
+    void testExcludeFromDocumentation() throws Exception {
         File file = File.createTempFile("rest_v0_", ".html");
         OpenApiSpecGenerator.createDocumentationFile(
                 new TestExcludeDocumentingRestEndpoint(), RestAPIVersion.V0, file.toPath());
         String actual = FileUtils.readFile(file, "UTF-8");
 
-        assertThat(actual, containsString("/test/empty1"));
-        assertThat(actual, containsString("This is a testing REST API."));
-        assertThat(actual, containsString("/test/empty2"));
-        assertThat(actual, containsString("This is another testing REST API."));
-        assertThat(actual, not(containsString("/test/exclude1")));
-        assertThat(
-                actual,
-                not(
-                        containsString(
-                                "This REST API should not appear in the generated documentation.")));
-        assertThat(actual, not(containsString("/test/exclude2")));
-        assertThat(
-                actual,
-                not(
-                        containsString(
-                                "This REST API should also not appear in the generated documentation.")));
+        assertThat(actual).contains("/test/empty1");
+        assertThat(actual).contains("This is a testing REST API.");
+        assertThat(actual).contains("/test/empty2");
+        assertThat(actual).contains("This is another testing REST API.");
+        assertThat(actual).doesNotContain("/test/exclude1");
+        assertThat(actual)
+                .doesNotContain("This REST API should not appear in the generated documentation.");
+        assertThat(actual).doesNotContain("/test/exclude2");
+        assertThat(actual)
+                .doesNotContain(
+                        "This REST API should also not appear in the generated documentation.");
     }
 
     private static class TestExcludeDocumentingRestEndpoint implements DocumentingRestEndpoint {
