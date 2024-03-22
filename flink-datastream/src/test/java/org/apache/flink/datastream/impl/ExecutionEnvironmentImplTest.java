feeba08cac259c34270a87b41618df261249d857
Weijie Guo <reswqa@163.com>
2024-03-22 17:27:27 +0800

    [FLINK-34548][API] Supports FLIP-27 Source

--- flink-datastream/src/test/java/org/apache/flink/datastream/impl/ExecutionEnvironmentImplTest.java
+++ flink-datastream/src/test/java/org/apache/flink/datastream/impl/ExecutionEnvironmentImplTest.java
@@ -20,11 +20,19 @@ package org.apache.flink.datastream.impl;
 
 import org.apache.flink.api.common.RuntimeExecutionMode;
 import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.connector.dsv2.DataStreamV2SourceUtils;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.core.execution.DefaultExecutorServiceLoader;
 import org.apache.flink.datastream.api.ExecutionEnvironment;
+import org.apache.flink.datastream.api.stream.NonKeyedPartitionStream;
+import org.apache.flink.datastream.impl.stream.StreamTestUtils;
+import org.apache.flink.streaming.api.graph.StreamGraph;
+import org.apache.flink.streaming.api.graph.StreamNode;
 
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -66,5 +74,22 @@ class ExecutionEnvironmentImplTest {
                         new DefaultExecutorServiceLoader(), new Configuration(), null);
         env.setExecutionMode(RuntimeExecutionMode.BATCH);
         assertThat(env.getExecutionMode()).isEqualTo(RuntimeExecutionMode.BATCH);
+    }
+
+    @Test
+    void testFromSource() {
+        ExecutionEnvironmentImpl env =
+                new ExecutionEnvironmentImpl(
+                        new DefaultExecutorServiceLoader(), new Configuration(), null);
+        NonKeyedPartitionStream<Integer> source =
+                env.fromSource(
+                        DataStreamV2SourceUtils.fromData(Arrays.asList(1, 2, 3)), "test-source");
+        source.process(new StreamTestUtils.NoOpOneInputStreamProcessFunction());
+        StreamGraph streamGraph = StreamTestUtils.getStreamGraph(env);
+        Collection<StreamNode> nodes = streamGraph.getStreamNodes();
+        assertThat(nodes).hasSize(2);
+        Collection<Integer> sourceIDs = streamGraph.getSourceIDs();
+        StreamNode sourceNode = nodes.iterator().next();
+        assertThat(sourceIDs).containsExactly(sourceNode.getId());
     }
 }
