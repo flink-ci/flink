887cfb998873781b45e9d82411e3f9c9abc4aef9
Jeyhun Karimov <je.karimov@gmail.com>
2024-03-27 21:27:11 +0100

    [FLINK-31497][table] Drop the deprecated CatalogViewImpl

--- flink-python/pyflink/table/catalog.py
+++ flink-python/pyflink/table/catalog.py
@@ -765,9 +765,14 @@ class CatalogBaseTable(object):
         assert properties is not None
 
         gateway = get_gateway()
+
         return CatalogBaseTable(
-            gateway.jvm.org.apache.flink.table.catalog.CatalogViewImpl(
-                original_query, expanded_query, schema._j_table_schema, properties, comment))
+            gateway.jvm.org.apache.flink.table.catalog.CatalogView.of(
+                schema._j_table_schema.toSchema(),
+                comment,
+                original_query,
+                expanded_query,
+                properties))
 
     @staticmethod
     def _get(j_catalog_base_table):
